258e65b467df6adfe1dde52b90e1de5e86476f8e
Bojan Micic <bojanmicic89@gmail.com>
2017-01-14 13:02:24 +0100

    removed some extra lines

--- Tests/test-spec.js
+++ Tests/test-spec.js
@@ -40,7 +40,6 @@ describe('deezer', function() {
     expect(todoList.get(2).getText()).toEqual('write first protractor test');
 
 
-
     todoList.get(2).element(by.css('input')).click();
     var completedAmount = element.all(by.css('.done-true'));
     expect(completedAmount.count()).toEqual(2);
